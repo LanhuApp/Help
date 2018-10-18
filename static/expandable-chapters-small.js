31d1032afd1252803efc2290d03dc8a55c8be32e
magic <290834092@qq.com>
2018-10-18 10:31:25 +0800

    滚动列表跟随

--- static/expandable-chapters-small.js
+++ static/expandable-chapters-small.js
@@ -5,6 +5,72 @@ require(['gitbook', 'jQuery'], function(gitbook, $) {
       TRIGGER_TEMPLATE = '<i class="exc-trigger fa"></i>',
       LS_NAMESPACE = 'expChapters';
   
+
+
+  function scrollFollow(){
+    var _debounce = function(func, wait, immediate) {
+      var timeout, args, context, timestamp, result;
+    
+      var later = function() {
+        // 据上一次触发时间间隔
+        var last = new Date().getTime() - timestamp;
+    
+        // 上次被包装函数被调用时间间隔last小于设定时间间隔wait
+        if (last < wait && last > 0) {
+          timeout = setTimeout(later, wait - last);
+        } else {
+          timeout = null;
+          // 如果设定为immediate===true，因为开始边界已经调用过了此处无需调用
+          if (!immediate) {
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+          }
+        }
+      };
+      
+      return function() {
+        context = this;
+        args = arguments;
+        timestamp = new Date().getTime();
+        var callNow = immediate && !timeout;
+        // 如果延时不存在，重新设定延时
+        if (!timeout) timeout = setTimeout(later, wait);
+        if (callNow) {
+          result = func.apply(context, args);
+          context = args = null;
+        }
+    
+        return result;
+      };
+    };
+
+
+    var clientHeight = $(window).outerHeight();
+    window.onresize = function(){
+      clientHeight = $(window).outerHeight();
+    }
+
+    var listWrap = $('.book-summary');
+
+    $('.body-inner')[0].onscroll = _debounce(function(){
+      
+      var tagOffsetTop = $('.active').offset().top;
+      if(tagOffsetTop>clientHeight || tagOffsetTop<0){
+        var toTop = listWrap.scrollTop()+tagOffsetTop-clientHeight/3;
+        listWrap.scrollTop(toTop);
+      }
+
+    },500,false)
+
+
+
+  }    
+
+  scrollFollow();
+
+
+
+
   function createNavList(){
 
     var navList ='';
@@ -206,8 +272,8 @@ require(['gitbook', 'jQuery'], function(gitbook, $) {
   }
   
 
-
   gitbook.events.bind('page.change', function() {
     init(); 
+    scrollFollow();
   }); 
 });
